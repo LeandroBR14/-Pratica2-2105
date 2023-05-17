10f317fe59346e4f19c30f0a9ef5a20f73708daa
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-17 20:29:21 -0300

    Problema4 finalizado

---
+++ Problema4.java
@@ -0,0 +1,29 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 4. Faça um programa que leia um número inteiro e mostre uma
+mensagem indicando se este número é par ou ímpar e se é
+positivo ou negativo.
+*/
+
+import java.util.Scanner;
+
+class Problema4{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = teclado.nextInt();
+
+        if (numero > 0) {
+            System.out.print("O  número é positivo");
+        } if (numero < 0) {
+            System.out.print("O  número é negativo");
+        } if (numero % 2 == 0) {
+            System.out.println(" e par.");
+        } else {
+            System.out.println(" e ímpar.");
+        }
+
+        teclado.close();
+    }
+}
