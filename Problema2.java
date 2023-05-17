d0c34ae2b662ad27c56ff1aaec8ed415a9e9139d
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-17 07:47:50 -0300

    Problema2 finalizado

---
+++ Problema2.java
@@ -0,0 +1,44 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 2. Construa um programa que receba como entrada três valores A, B
+e C e os imprima em ordem crescente.
+* Data de entrega: 21/5/2023
+*/
+
+import java.util.Scanner;
+
+class Problema2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um primeiro número: ");
+        double a = teclado.nextDouble();
+
+        System.out.print("Digite um segundo número: ");
+        double b = teclado.nextDouble();
+
+        System.out.print("Digite um terceiro número: ");
+        double c = teclado.nextDouble();
+
+        if (a <= b && a <= c) {
+            if (b <= c) {
+                System.out.println(a + ", " + b + ", " + c);
+            } else {
+                System.out.println(a + ", " + c + ", " + b);
+            }
+        } else if (b <= a && b <= c) {
+            if (a <= c) {
+                System.out.println(b + ", " + a + ", " + c);
+            } else {
+                System.out.println(b + ", " + c + ", " + a);
+            }
+        } else {
+            if (a <= b) {
+                System.out.println(c + ", " + a + ", " + b);
+            } else {
+                System.out.println(c + ", " + b + ", " + a);
+            }
+            teclado.close();
+        }
+    }
+}
