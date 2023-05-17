1a7cd78b762556dc0c41c070f789a34a306d29a5
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-17 07:43:26 -0300

    first commit

---
+++ Problema1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Problema1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe um primeiro número: ");
+        double n1 = teclado.nextDouble();
+        System.out.print("Informe um segundo número: ");
+        double n2 = teclado.nextDouble();
+
+        if (n1 > n2) {
+            System.out.println(n1);
+        } else if (n2 > n1) {
+            System.out.println(n2);
+        } else if (n1 == n2) {
+            System.out.println("Os números são iguais.");
+        }
+        teclado.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Atividade prática 2
+```
+Professor: Flávio Augusto Rezende Calado
+Nome do Aluno: Leandro Scalabrini
+RA: 32318945
+UC: Programação de Soluções Computacionais
+Data de entrega: 21/5/2023
+```
