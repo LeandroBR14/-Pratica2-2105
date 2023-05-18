3895766bd76be3f44e19e4b098f63039b93bd3e9
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-17 22:02:08 -0300

    Problema13 finalizado

--- Problema13.java
+++ Problema13.java
@@ -1,3 +1,18 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 13. Faça um programa que some os números de 1 a 100 e imprima
+somente o valor total da soma (utilize while).
+* Data de entrega: 21/5/2023
+*/
+
 public class Problema13 {
-    
+    public static void main(String[] args) {
+        int i = 0;
+        int soma = 0;
+        while (i <= 100) {
+            soma += i;
+            i++;
+        }
+        System.out.println("A soma dos números de 1 a 100 é: " + soma);
+    }
 }
