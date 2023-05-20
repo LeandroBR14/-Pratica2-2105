28b6cd041e643f7d81dc60de5d897b7df8b1d7b2
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-20 20:46:19 -0300

    Problema19 finalizado

---
+++ Problema19.java
@@ -0,0 +1,25 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 19. Um determinado gás duplica seu volume a cada segundo.
+Dada um volume inicial, em centímetros cúbicos, digitado pelo
+usuário faça um programa que determine o tempo necessário para
+que esse volume se torne maior que 1000 centímetros cúbicos.
+(utilize while)
+* Data de entrega: 21/5/2023
+*/
+
+import java.util.Scanner;
+class Problema19 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o volume do gás em cm³: ");
+        int volumeAtual = teclado.nextInt();
+        teclado.close();
+        int tempo = 0;
+        while (volumeAtual <= 1000) {
+            volumeAtual *= 2;   
+            tempo++;
+        }
+        System.out.printf("É necessário %d segundos para que o volume desse gás chegue em %dcm³", tempo, volumeAtual);
+    }
+}
