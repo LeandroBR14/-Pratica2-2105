b292830ab0a604c27180db025b84f5977ab9aa27
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-20 20:16:19 -0300

    Problema16 finalizado

---
+++ Problema16.java
@@ -0,0 +1,22 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 16. Faça um programa que solicite ao usuário que digite um
+número até que ele digite um número menor que 0 (utilize
+while)
+* Data de entrega: 21/5/2023
+*/
+
+import java.util.Scanner;
+class Atividade4{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero = 0;
+
+        while (numero >= 0) {
+            System.out.print("Digite um número qualquer: ");
+            numero = teclado.nextInt();
+        }
+        System.out.println("O número digitado é menor do que 0.");
+        teclado.close();
+    }
+}
